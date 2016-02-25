fbb58c712fbcf3a9b706b95d4098763c3a851fbe
GarethPGH <garethlwarren@users.noreply.github.com>
2016-02-24 20:43:28 -0500

    I know it is a horrid mess:
    Updates: Attempting to OOP with creation of a list of TaskObjects to give ToDoList persisting data.
    
    Commented code to test if it works. Comments on adding a UI. I want it to be cross-platform instead of Windows Forms specific. I found GTKMM for that, but I recently stumbled across Mongoose and will probably be using that.
    
    Not shown: a failed attempt to print ToDoList. My current focus is testing if the list works. Once this is verified all the test code will be removed and I will work on the UI.

--- ReintrosareNiceToDoList/ReintrosareNiceToDoList/ToDo.cpp
+++ ReintrosareNiceToDoList/ReintrosareNiceToDoList/ToDo.cpp
@@ -3,37 +3,102 @@
 #include <sstream>
 #include <fstream>
 #include "resultString.h"
+#include <Windows.h>
+#include <list>
+#include "resource.h"
+
 
 using namespace std;
-
-
-//My resultString.H and resultString.cpp files are buggered. Fix it.
+class TaskObject;
+//void printList(std::list<TaskObject>);
 
 
 int main()
 {
+	//should I create a Class?
 
-	int         loadingString;
-	int         taskNum;
+	class TaskObject {
 
+	public:
+		int listNum;
+		string taskName;
+
+		TaskObject() {
+			listNum = 1;
+			taskName = "sometask";
+		}
+		void TaskAssign(int tNum, string tString) {
+			listNum = tNum;
+			taskName = tString;
+			
+		}
+		//^^^^^^^^^Fix this ^^^^^^^^^^^^^
+	};
+	//wrap this into a textbox of some kind, add a submit button
 	
+	
+	int loadingString;
+	int taskNum;
+
+	//create a list of taskObject objects to store values.
 	cout << "Enter your TaskList" << endl;
 	taskNum = 1;
 	string taskItem;
+	//list is global so that other functions have access.
+
+	std::list<TaskObject> ToDoList;
+	ToDoList.clear;
+	//create a new list of TaskList objects. Figure out how to make it varying in size or assign a large number of spaces.
 
 			for (loadingString = 0; loadingString < taskNum; loadingString++)
 			{
 				taskItem = resultString();
-				cout << loadingString + 1 << ": ";
-				cout << taskItem << endl;
-				taskNum++;
 				
+				TaskObject TaskList;
+				//Declare a new object of type TaskObject named TaskList
+				
+
+				//cout << to_string(TaskList.listNum) + " : " + TaskList.taskName << endl;
+				//cout << to_string(taskNum) +" ";
+				//cout << taskItem << endl;
+
+				//assign data to TaskList object
+				TaskList.TaskAssign(taskNum, taskItem);
+				
+				//Data should now persist in the tasklist
+				//cout << to_string(TaskList.listNum) + " : " + TaskList.taskName << endl;
+				//cout << to_string(taskNum)  + " ";
+				//cout << taskItem << endl;
+				
+				taskNum++;
+
 				if (taskItem == "complete") {
 					break;
 				}
+				//assign TaskList object to ToDoList
+				ToDoList.resize(1);
+				ToDoList.push_back(TaskList);
+				ToDoList.get_allocator;
+				
+				//why isnt this working?
+				//Create persistent storage container:List. 
+				//print list to assert items were added to list... 
+				
+				
 			}
-			//I DID IT!!
-	return 0;
-}
 
-//Lets put a GUI on this bitch
+			//Put TaskObject assignment into TaskList as TaskList[n].
+
+			//This should be done at the end of assigning task items.
+			
+			
+			//Optional Done button should call a method that displays a strike through of already done items. Or deletes done items
+			//Optional delete button to delete items that are deemed not worthy of being done.
+
+			//Close button to call finished. Ie: set taskItem to complete.
+	return 0;
+
+
+
+	//Figure out how to add the GTK libraries to build cross platform UI to this project. They can be located in D:/Gtkmm
+}
