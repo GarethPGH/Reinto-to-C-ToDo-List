7b14a2f5385ddd2a1d891743a2030477aa77f01f
GarethPGH <garethlwarren@users.noreply.github.com>
2016-03-06 21:38:42 -0500

    Added a ToDoList vector to store TaskList objects as persistent data.

--- ReintrosareNiceToDoList/ReintrosareNiceToDoList/ToDo.cpp
+++ ReintrosareNiceToDoList/ReintrosareNiceToDoList/ToDo.cpp
@@ -4,39 +4,49 @@
 #include <fstream>
 #include "resultString.h"
 #include <Windows.h>
-#include <list>
+#include <vector>
 #include "resource.h"
 
-
 using namespace std;
-class TaskObject;
-//void printList(std::list<TaskObject>);
 
+class TaskObject {
+
+public:
+	int listNum;
+	string taskName;
+	
+
+	TaskObject() {
+		listNum = 1;
+		taskName = "somestring";
+		
+	}
+
+	TaskObject(int tNum, string tString) {
+		listNum = tNum; 
+		taskName = tString;
+	}
+
+};
+
+// Cant seem to figure out how to overload the << operator to accept my TaskObject values.
+ostream& operator<< (ostream& stream, const TaskObject& a)///T is undefined but should be template letter?
+{
+
+	stream << to_string(a.listNum) + " : " + a.taskName ;
+	stream << endl;
+
+
+	return stream;
+}
+
+inline bool operator== (TaskObject task1, TaskObject task2) {
+	if ((task1.listNum == task1.listNum) & (task1.taskName == task2.taskName))
+	return true;
+}
 
 int main()
 {
-	//should I create a Class?
-
-	class TaskObject {
-
-	public:
-		int listNum;
-		string taskName;
-
-		TaskObject() {
-			listNum = 1;
-			taskName = "sometask";
-		}
-		void TaskAssign(int tNum, string tString) {
-			listNum = tNum;
-			taskName = tString;
-			
-		}
-		//^^^^^^^^^Fix this ^^^^^^^^^^^^^
-	};
-	//wrap this into a textbox of some kind, add a submit button
-	
-	
 	int loadingString;
 	int taskNum;
 
@@ -45,60 +55,64 @@ int main()
 	taskNum = 1;
 	string taskItem;
 	//list is global so that other functions have access.
-
-	std::list<TaskObject> ToDoList;
-	ToDoList.clear;
+	std::vector<TaskObject> ToDoList;
+	ToDoList.clear();
+	ToDoList.resize(1);
 	//create a new list of TaskList objects. Figure out how to make it varying in size or assign a large number of spaces.
 
-			for (loadingString = 0; loadingString < taskNum; loadingString++)
-			{
-				taskItem = resultString();
-				
-				TaskObject TaskList;
-				//Declare a new object of type TaskObject named TaskList
-				
+	for (loadingString = 0; loadingString < taskNum; loadingString++)
+	{
+		unsigned int size = ToDoList.capacity();
+		for (unsigned int listloop = 0; listloop < size; listloop++) {
 
-				//cout << to_string(TaskList.listNum) + " : " + TaskList.taskName << endl;
-				//cout << to_string(taskNum) +" ";
-				//cout << taskItem << endl;
+		taskItem = resultString();
+		//set value of taskItem
+		TaskObject TaskListObject(taskNum, taskItem);
+		//pass values to object constructor; should assign to object properties
+		
+		if (taskItem != TaskListObject.taskName) {
+			cout << "object not assigned";
+			break;
+		}
+		//This if statement is a placeholder for a future UI exit button.
+		else if (taskItem == "complete"){
+			break;
+		}
+		taskNum++;
+		//increment tasknum
+		ToDoList.resize(taskNum);
+		//resize the vector/list
 
-				//assign data to TaskList object
-				TaskList.TaskAssign(taskNum, taskItem);
-				
-				//Data should now persist in the tasklist
-				//cout << to_string(TaskList.listNum) + " : " + TaskList.taskName << endl;
-				//cout << to_string(taskNum)  + " ";
-				//cout << taskItem << endl;
-				
-				taskNum++;
-
-				if (taskItem == "complete") {
-					break;
-				}
-				//assign TaskList object to ToDoList
-				ToDoList.resize(1);
-				ToDoList.push_back(TaskList);
-				ToDoList.get_allocator;
-				
-				//why isnt this working?
-				//Create persistent storage container:List. 
-				//print list to assert items were added to list... 
-				
-				
-			}
-
-			//Put TaskObject assignment into TaskList as TaskList[n].
-
-			//This should be done at the end of assigning task items.
+		//Double nesting loop to prevent it from changing my previous ToDoList values.
+	
+			ToDoList[listloop] = TaskListObject;
+			TaskObject* ptr = &ToDoList[listloop];
+			TaskObject taskAssigned = *ptr;
+			listloop++;
+			ToDoList.reserve(listloop);
+			//resize my vector again.
+			ptr++;
+			//increment pointer
 			
+			//cout << taskAssigned;
+			//print indexed value, should match TaskObject.
 			
-			//Optional Done button should call a method that displays a strike through of already done items. Or deletes done items
-			//Optional delete button to delete items that are deemed not worthy of being done.
+			if (taskAssigned == TaskListObject) {
+				cout << "TaskListObject has been assigned to TaskList";
+			}	
+			
+		}
 
-			//Close button to call finished. Ie: set taskItem to complete.
+	//print entire ToDoList
+
+	}
+
+	//Optional Done button should call a method that displays a strike through of already done items. Or deletes done items
+	//Optional delete button to delete items that are deemed not worthy of being done.
+
+	//Close button to call finished. Ie: set taskItem to complete.
 	return 0;
-
-
-
-	//Figure out how to add the GTK libraries to build cross platform UI to this project. They can be located in D:/Gtkmm
 }
+
+
+	//UI Gtkmm or Mongoose.
